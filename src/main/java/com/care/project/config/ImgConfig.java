7a488ede19d4d92995301861e524ac2125dff61a
hoyoung <skghdud024@naver.com>
2025-02-04 00:18:10 +0900

    fix : porm.xml 수정 및 각 controller 코드 수정

--- src/main/java/com/care/project/config/ImgConfig.java
+++ src/main/java/com/care/project/config/ImgConfig.java
@@ -1,4 +1,4 @@
-package com.care.project.main.controller;
+package com.care.project.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
