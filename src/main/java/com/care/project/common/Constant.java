a93c3f92df051550d859716c5ba8b7d7680cd40d
seongho <412jsh@naver.com>
2025-02-07 15:14:12 +0900

    예매좌석 진행중

--- src/main/java/com/care/project/common/Constant.java
+++ src/main/java/com/care/project/common/Constant.java
@@ -2,7 +2,7 @@ package com.care.project.common;
 
 public class Constant {
 	 public static class Success{
-	        public static final int SUCCESS_CODE = 1000;
+	        public static final int SUCCESS_CODE = 200;
 	    }
 
 }
